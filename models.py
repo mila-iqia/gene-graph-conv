ca33abc6f8caef849bbd70b89ee6a3fa07fa8678
Francis Dutil <frdutil@gmail.com>
2017-12-15 15:45:20 -0500

    They were some bug in lcg

--- models.py
+++ models.py
@@ -36,7 +36,7 @@ class CGN(nn.Module):
         print "Constructing the network..."
         layers = []
         for c_in, c_out in zip(dims[:-1], dims[1:]):
-            layer = nn.Conv1d(c_in, c_out, 1, bias=True)
+            layer = nn.Conv1d(c_in, c_out, 1, bias=False)
             layers.append(layer)
         self.my_layers = nn.ModuleList(layers)
 
@@ -148,7 +148,7 @@ class MLP(nn.Module):
         return x
 
 class LCG(nn.Module):
-    def __init__(self,input_dim, A, channels=16, out_dim=2, on_cuda=False, num_layers = 1, arg_max = -200):
+    def __init__(self,input_dim, A, channels=16, out_dim=2, on_cuda=False, num_layers = 1, arg_max = -1):
         super(LCG, self).__init__()
 
         self.my_layers = []
@@ -157,82 +157,67 @@ class LCG(nn.Module):
         self.nb_nodes = A.shape[0]
         self.num_layers = num_layers
 
-        self.channels = channels
-        #dims = [input_dim] + channels
-
-        #import ipdb; ipdb.set_trace()
+        self.nb_channels = [channels] # we only support 1 layer for now.
 
         print "Constructing the network..."   
         self.max_edges = sorted((A > 0.).sum(0))[arg_max]
-        #self.max_edges = max((A > 0.).sum(0))
 
         print "Each node will have {} edges.".format(self.max_edges)
 
 
+        # Get the list of all the edges. All the first index is 0, we fix that later
         edges_np = [np.asarray(np.where(A[i:i+1] > 0.)).T for i in range(len(A))]
-        edges_np = [np.concatenate([x, [[0, i]] * (self.max_edges - len(x))]) if len(x) < self.max_edges
-                    else x[:self.max_edges] if len(x) > self.max_edges # Some Nodes has to many connection!
+
+
+        # pad the edges, so they all nodes have the same number of edges. help to automate everything.
+        edges_np = [np.concatenate([x, [[0, self.nb_nodes]] * (self.max_edges - len(x))]) if len(x) < self.max_edges
+                    else x[:self.max_edges] if len(x) > self.max_edges # Some Nodes have to many connection!
                     else x
                     for i, x in enumerate(edges_np)]
 
+        # fix the index that was all 0.
         for i in range(len(edges_np)):
             edges_np[i][:, 0] = i
 
+
         edges_np = np.array(edges_np).reshape(-1, 2)
-        edges_np = edges_np[:, 0:1]
-        print edges_np.shape
+        edges_np = edges_np[:, 1:2]
 
         self.edges = torch.LongTensor(edges_np)
         self.super_edges = torch.cat([self.edges] * channels)
 
-        self.my_weights = [nn.Parameter(torch.rand(self.edges.shape[0], channels), requires_grad=True)]
-        self.my_bias = [nn.Parameter(torch.zeros(self.nb_nodes, channels), requires_grad=True)]
+        # we add a weight the fake node (that we introduced in the padding)
+        self.my_weights = [nn.Parameter(torch.rand(self.edges.shape[0], channels), requires_grad=True)] # TODO add layer
+
+        # But that weight to zero
+        #for w in self.my_weights:
+        #    w.data[-1] = 0.
 
         last_layer = nn.Linear(input_dim * channels, out_dim)
         self.my_logistic_layers = nn.ModuleList([last_layer])
 
-        #self.edge_selector = [np.where(edges_np[:,0] == i)[0] for i in range(input_dim)]
         self.register_buffer('edges', self.edges)
 
         print "Done!"
 
-    def GraphConv(self, x, edges, batch_size, weights, bias):
+    def GraphConv(self, x, edges, batch_size, weights):
         
         edges = edges.contiguous().view(-1)
+        useless_node = torch.zeros(x.size(0), 1, x.size(2))
 
         if self.on_cuda:
             edges = edges.cuda()
             weights = weights.cuda()
-            bias = bias.cuda()
+            useless_node = useless_node.cuda()
+
+        x = torch.cat([x, useless_node], 1) # add a random filler node
+        tocompute = torch.index_select(x, 1, edges).view(batch_size, -1, weights.size(-1))
+
 
         #import ipdb; ipdb.set_trace()
 
-        # We have some memory issues when using big
-        # convs = []
-        # nb = 100000
-        # for i in range(edges.size(0)/nb + 1):
-        #
-        #     if i* nb >= edges.size(0):
-        #         break
-        #
-        #
-        #     sub_edges = edges[i*nb:min((i+1)*nb, edges.size(0))]
-        #     sub_weight = weights[i*nb/weights.size(-1):min((i+1)*nb, edges.size(0))/weights.size(-1)]
-        #
-        #     tocompute = torch.index_select(x, 1, sub_edges).view(batch_size, -1, sub_weight.size(-1))#, 2)
-        #
-        #     print i*nb,(i+1)*nb, weights.size(0)
-        #     conv = tocompute*sub_weight
-        #     convs.append(conv)
-        #
-        # import ipdb;
-        # ipdb.set_trace()
-        # conv = torch.cat(convs, dim=1)
-
-        tocompute = torch.index_select(x, 1, edges).view(batch_size, -1, weights.size(-1))
         conv = tocompute * weights
         conv = conv.view(-1, self.nb_nodes, self.max_edges, weights.size(-1)).sum(2)
-        #conv = conv + bias
         return F.tanh(conv)
 
     def forward(self, x):
@@ -243,7 +228,8 @@ class LCG(nn.Module):
         if self.on_cuda:
             edges = edges.cuda()
 
-        x = self.GraphConv(x, edges.data, nb_examples, self.my_weights[0], self.my_bias[0])
+
+        x = self.GraphConv(x, edges.data, nb_examples, self.my_weights[0]) # TODO: add more layer
         x = self.my_logistic_layers[-1](x.view(nb_examples, -1))
         x = F.softmax(x)
 
@@ -263,10 +249,12 @@ def get_model(opt, dataset, nb_class):
     num_channel = opt.num_channel
     num_layer = opt.num_layer
     on_cuda = opt.cuda
+    skip_connections = opt.skip_connections
 
     if model == 'cgn':
         # To have a feel of the model, please take a look at cgn.ipynb
-        my_model = CGN(dataset.nb_nodes, 1, [num_channel] * num_layer, dataset.get_adj(), nb_class, on_cuda=on_cuda)
+        my_model = CGN(dataset.nb_nodes, 1, [num_channel] * num_layer, dataset.get_adj(), nb_class,
+                       on_cuda=on_cuda, add_residual=skip_connections)
 
     elif model == 'mlp':
         my_model = MLP(dataset.nb_nodes, [num_channel] * num_layer, nb_class, on_cuda=on_cuda)
