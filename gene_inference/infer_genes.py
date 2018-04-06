f5c545dbcb4291e9cf6c2bc51cf625d4073a4fe2
mweiss17 <martin.weiss@mail.mcgill.ca>
2018-04-06 16:32:01 -0400

    added more notebooks

--- gene_inference/infer_genes.py
+++ gene_inference/infer_genes.py
@@ -7,10 +7,10 @@ import pandas as pd
 from utils import get_second_degree
 
 
-def infer_gene(method, genes, gene_to_infer, g):
+def infer_gene(method, genes, gene_to_infer, g, num_samples, penalty=False):
     labels = [1 if x > genes[gene_to_infer].mean() else 0 for x in genes[gene_to_infer]]
 
-    # TODO: fix this hack. There are some genes
+    # TODO: fix this hack -- sometimes we get an sklearn error when there is only 1 class label in a batch
     if sum(labels) < 2:
         labels[0] = 1
 
@@ -19,17 +19,17 @@ def infer_gene(method, genes, gene_to_infer, g):
     first_degree_neighbors = temp_genes.loc[:, list(g.neighbors(gene_to_infer))].dropna(axis=1)
     second_degree_neighbors = temp_genes.loc[:, list(get_second_degree(gene_to_infer, g))].dropna(axis=1)
 
-    full_results = method(temp_genes, labels, 10, 1000)
+    full_results = method(temp_genes, labels, 10, num_samples, penalty=penalty)
 
     if len(first_degree_neighbors.columns) == 0:
         first_degree_results = (.5, .0)
     else:
-        first_degree_results = method(first_degree_neighbors, labels, 10, 1000)
+        first_degree_results = method(first_degree_neighbors, labels, 10, num_samples, penalty=penalty)
 
     if len(second_degree_neighbors.columns) == 0:
         second_degree_results = (.5, .0)
     else:
-        second_degree_results = method(second_degree_neighbors, labels, 10, 1000)
+        second_degree_results = method(second_degree_neighbors, labels, 10, num_samples, penalty=penalty)
 
     first_degree_diff = full_results[0] - first_degree_results[0]
     second_degree_diff = full_results[0] - second_degree_results[0]
--- gene_inference/models.py
+++ gene_inference/models.py
@@ -5,13 +5,16 @@ import matplotlib, matplotlib.pyplot as plt
 import networkx as nx
 import pandas as pd
 
-def lr(df, labels, trials, samples, train_ratio=0.5):
+
+def lr(df, labels, trials, train_size, penalty=False):
     scores = []
     for i in range(trials):
         X_train, X_test, y_train, y_test = \
-        sklearn.model_selection.train_test_split(df[:samples], labels[:samples], stratify=labels[:samples], train_size=train_ratio, random_state=i)
+        sklearn.model_selection.train_test_split(df, labels, stratify=labels, train_size=train_size, random_state=i)
 
         model = sklearn.linear_model.LogisticRegression()
+        if penalty:
+            model = sklearn.linear_model.LogisticRegression(penalty='l1', tol=0.0001)
         model = model.fit(X_train, y_train)
 
         score = sklearn.metrics.roc_auc_score(y_test, model.predict(X_test))
@@ -19,27 +22,33 @@ def lr(df, labels, trials, samples, train_ratio=0.5):
     return np.round(np.mean(scores), 2),  np.round(np.std(scores),2)
 
 
-def mlp(df, labels, trials, samples, train_ratio=0.5):
+def mlp(df, labels, trials, train_size, penalty=False):
     # Try with only 1 layer, and with regularization
     scores = []
     for i in range(trials):
         X_train, X_test, y_train, y_test = \
-        sklearn.model_selection.train_test_split(df[:samples], labels[:samples], stratify=labels[:samples], train_size=train_ratio, random_state=i)
+        sklearn.model_selection.train_test_split(df, labels, stratify=labels, train_size=train_size, random_state=i)
 
         model = sklearn.neural_network.MLPClassifier()
+        if penalty:
+            model = sklearn.neural_network.MLPClassifier()
+
         model = model.fit(X_train, y_train)
 
         score = sklearn.metrics.roc_auc_score(y_test, model.predict(X_test))
         scores.append(score)
     return np.round(np.mean(scores), 2),  np.round(np.std(scores),2)
 
-def decision_tree(df, labels, trials, samples, train_ratio=0.5):
+def decision_tree(df, labels, trials, train_size, penalty=False):
     scores = []
     for i in range(trials):
         X_train, X_test, y_train, y_test = \
-        sklearn.model_selection.train_test_split(df[:samples], labels[:samples], stratify=labels[:samples], train_size=train_ratio, random_state=i)
+        sklearn.model_selection.train_test_split(df, labels, stratify=labels, train_size=train_size, random_state=i)
 
         model = sklearn.tree.DecisionTreeClassifier()
+        if penalty:
+            model = sklearn.tree.DecisionTreeClassifier()
+
         model = model.fit(X_train, y_train)
 
         score = sklearn.metrics.roc_auc_score(y_test, model.predict(X_test))
