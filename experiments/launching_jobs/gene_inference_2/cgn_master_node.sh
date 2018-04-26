8b900a91ee6b9ba91e9338efa8760d5550cab776
Francis Dutil <frdutil@gmail.com>
2018-04-26 13:42:08 -0400

    Adding sample scripts for experience in low data regime

--- experiments/launching_jobs/gene_inference_2/cgn_master_node.sh
+++ experiments/launching_jobs/gene_inference_2/cgn_master_node.sh
@@ -8,8 +8,8 @@ cd ../..
 
 # CGN
 # Without dropout
-./launch_all_my_stuff.sh " --model cgn --dataset dgex --graph pancan --nb-examples 2000 --training-mode gene-inference --num-layer 2 --use-emb 128 --cuda --data-file bgedv2.hdf5 --data-dir /data/lisa/data/genomics/D-GEX/ --epoch 500 --master-nodes 1 --num-channel 128 --tensorboard-dir ~/milatmp1/transcriptome/graph/experience/set_1" 1 $exp_dir
-./launch_all_my_stuff.sh " --model cgn --dataset dgex --graph pancan --nb-examples 2000 --training-mode gene-inference --num-layer 2 --use-emb 128 --cuda --data-file bgedv2.hdf5 --data-dir /data/lisa/data/genomics/D-GEX/ --epoch 500 --master-nodes 10 --num-channel 128 --tensorboard-dir ~/milatmp1/transcriptome/graph/experience/set_1" 1 $exp_dir
+#./launch_all_my_stuff.sh " --model cgn --dataset dgex --graph pancan --nb-examples 2000 --training-mode gene-inference --num-layer 2 --use-emb 128 --cuda --data-file bgedv2.hdf5 --data-dir /data/lisa/data/genomics/D-GEX/ --epoch 500 --master-nodes 1 --num-channel 128 --tensorboard-dir ~/milatmp1/transcriptome/graph/experience/set_1" 1 $exp_dir
+#./launch_all_my_stuff.sh " --model cgn --dataset dgex --graph pancan --nb-examples 2000 --training-mode gene-inference --num-layer 2 --use-emb 128 --cuda --data-file bgedv2.hdf5 --data-dir /data/lisa/data/genomics/D-GEX/ --epoch 500 --master-nodes 10 --num-channel 128 --tensorboard-dir ~/milatmp1/transcriptome/graph/experience/set_1" 1 $exp_dir
 ./launch_all_my_stuff.sh " --model cgn --dataset dgex --graph pancan --nb-examples 2000 --training-mode gene-inference --num-layer 2 --use-emb 64 --cuda --data-file bgedv2.hdf5 --data-dir /data/lisa/data/genomics/D-GEX/ --epoch 500 --master-nodes 100 --num-channel 64 --tensorboard-dir ~/milatmp1/transcriptome/graph/experience/set_1" 1 $exp_dir
 
 
