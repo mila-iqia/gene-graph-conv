f0ff78c8c85cf0da7a65a814403c489c53011b4c
mweiss17 <martin.weiss@mail.mcgill.ca>
2018-09-19 17:05:50 -0400

    updating launch scripts

---
+++ experiments/launch_scripts/launch_all_fig_5_jobs.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+
+# Sometimes useful on the MILA cluster
+# export HOME=`getent passwd $USER | cut -d':' -f6`
+# source activate pytorch0.2
+
+# EXAMPLES OF HOW TO CALL THIS FILE, note that you should run this from the root of the project
+# genes=("RPL13" "HLA-B" "S100A9" "IFIT1" "RPL5" "RPS31" "ZFP82" "IL5" "DLGAP2")
+# ./experiments/launch_scripts/launch_all_fig_5_jobs.sh "${genes[*]}" data/graph/pancan-tissue-graph.hdf5  datastore/TCGA_tissue_ppi.hdf5 20 True
+
+# Pancan => Genemania
+# Kegg => Regnet
+# Get the inputs
+
+for i in "$@"
+do
+case $i in
+    --genes=*)
+    genes="${i#*=}"
+    ;;
+    --graph-path=*)
+    graph_path="${i#*=}"
+    ;;
+    --samples-path=*)
+    samples_path="${i#*=}"
+    ;;
+    --trials=*)
+    trials="${i#*=}"
+    ;;
+    --cuda=*)
+    cuda="${i#=}"
+    ;;
+esac
+done
+
+for gene in ${genes[@]}
+do
+    #sbatch --output $exp_dir"/slurm-%j.out" -x mila00 ./experiments/launch_scripts/launch_one_fig_5_job.sh $genes $bucket $exp_name $graph
+    ./experiments/launch_scripts/launch_one_fig_5_job.sh --gene=$gene --graph-path=$graph_path --samples-path=$samples_path --trials=$trials --cuda=$cuda
+done
---
+++ experiments/launch_scripts/launch_one_fig_5_job.sh
@@ -0,0 +1,48 @@
+#!/usr/bin/env bash
+
+gene=$1
+graph_path=$2
+samples_path=$3
+trials=$4
+cuda=$5
+echo $graph_path
+for i in "$@"
+do
+case $i in
+    --gene=*)
+    gene="${i#*=}"
+    ;;
+    --graph-path=*)
+    graph_path="${i#*=}"
+    ;;
+    --samples-path=*)
+    samples_path="${i#*=}"
+    ;;
+    --trials=*)
+    trials="${i#*=}"
+    ;;
+    --cuda=*)
+    cuda="${i#=}"
+    ;;
+esac
+done
+
+job_to_run="python -u run_exps_for_fig_5.py"
+job_to_run=$job_to_run" --gene="$gene
+if [ $cuda ]
+then
+    job_to_run=$job_to_run" --cuda ";
+fi
+if [ $graph_path ]
+then
+    job_to_run=$job_to_run" --graph-path="$graph_path;
+fi
+if [ $trials ]
+then
+    job_to_run=$job_to_run" --trials="$trials;
+fi
+if [ $trials ]
+then
+    job_to_run=$job_to_run" --samples-path="$samples_path;
+fi
+$job_to_run
