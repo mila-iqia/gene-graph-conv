d7aa44df6c5df148fa8cb69a076e89525b1b2ff1
mweiss17 <martin.weiss@mail.mcgill.ca>
2018-09-25 16:48:24 -0400

    fixing comment in launch script

--- experiments/launch_scripts/launch_all_fig_4_jobs.sh
+++ experiments/launch_scripts/launch_all_fig_4_jobs.sh
@@ -5,9 +5,9 @@
 # source activate pytorch0.2
 
 # EXAMPLES OF HOW TO CALL THIS FILE, note that you should run this from the root of the project
-# ./experiments/launch_scripts/launch_all_fig_4_jobs.sh 1 fig_4_genemania genemania
-# ./experiments/launch_scripts/launch_all_fig_4_jobs.sh 1 fig_4_regnet regnet
-# on Mila Cluster ./experiments/launch_scripts/launch_all_fig_4_jobs.sh 1 fig_4_genemania --graph-path=/data/lisa/data/genomics/graph/pancan-tissue-graph.hdf5 --samples-path=/data/lisa/data/genomics/TCGA/TCGA_tissue_ppi.hdf5 --cuda=True
+# ./experiments/launch_scripts/launch_all_fig_4_jobs.sh --num-buckets=1 --exp-name=genemania
+# ./experiments/launch_scripts/launch_all_fig_4_jobs.sh --num-buckets=1 --exp-name=genemania
+# on Mila Cluster ./experiments/launch_scripts/launch_all_fig_4_jobs.sh --num-buckets=1 --graph-path=/data/lisa/data/genomics/graph/pancan-tissue-graph.hdf5 --samples-path=/data/lisa/data/genomics/TCGA/TCGA_tissue_ppi.hdf5 --exp-name=genemania --cuda=True genemania
 
 # Pancan => Genemania
 # Kegg => Regnet
